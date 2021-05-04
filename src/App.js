5bfc8fb4f2264fcb9c6bf36887d8a2450d027896
HMABUHABIB <46394953+HMABUHABIB@users.noreply.github.com>
2021-05-04 18:02:50 +0200

    adding header component and links to home

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
 import HomePage from "./pages/homePage/HomePage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+
 function App() {
   return (
     <div>
+      <Header></Header>
+      <HashRouter basename={process.env.PUBLIC_URL}></HashRouter>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
+        <Route exact path="/shop" component={ShopPage} />
+        <Route component={() => <div>404 Not found</div>} />
       </Switch>
     </div>
   );
--- src/pages/homePage/HomePage.component.jsx
+++ src/pages/homePage/HomePage.component.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
 
-const HomePage = () => (
+const HomePage = (props) => (
   <div className="homepage">
     <Directory></Directory>
+    <button onClick={() => props.history.push("/shop")}>Go to shop</button>
   </div>
 );
 
